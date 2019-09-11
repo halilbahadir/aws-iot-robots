506234cb0029fe9d23c7e512d00a4da79f367a3e
Halil BAHADIR <bahadirhalil@gmail.com>
2019-09-11 21:14:15 +0300

    lab3 codes

---
+++ Scripts/lab3/lab3-1.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python
+
+# Lab 3-1
+# AWS Region Ireland olduguna emin olun.
+
+import sys
+import ssl
+from AWSIoTPythonSDK.MQTTLib import AWSIoTMQTTShadowClient
+import json
+import time
+import os 
+import random
+
+#MQTT Shadow Istemci olusturma ve guvenlik sertifikalarini tanimlama
+#Sertifa isimleri ile AWS IoT Thing indirdiginiz sertifika isimlerinin ayni olmasina dikkat edin.
+
+currentPath=os.getcwd()
+roboName=os.path.basename(currentPath)
+print("RoboName--> "+roboName)
+
+shadowClient=AWSIoTMQTTShadowClient(roboName)
+
+#Ayarlar sayfasindaki IoTEndpoint buraya ekleyin
+shadowClient.configureEndpoint("a100y3wur4j1gq-ats.iot.eu-west-1.amazonaws.com",8883)
+shadowClient.configureCredentials("../root-CA.crt","PrivateKey.pem","certificate.pem.crt")
+
+shadowClientHandler=shadowClient.createShadowHandlerWithName("Robo1",True) # Robo1 Thing adi olarak statik tanimli, parametre de olabilir.
+#JSON formatina encode eden fonksiyon
+def toJSON(string):
+        return json.dumps(string)
+
+shadowClient.toJSON=toJSON
+
+shadowMessage = {"state":{"desired":{"headLight":"On"}}}
+shadowMessage = json.dumps(shadowMessage)
+
+#Function to encode a payload into JSON
+def json_encode(string):
+        return json.dumps(string)
+
+#Function to print message
+def on_message(message, response, token):
+    print (message)
+
+shadowClientHandler.on_message= on_message
+shadowClientHandler.json_encode=json_encode
+
+#sending first shadow update
+shadowClient.connect()
+print ("Baglandi")
+shadowClientHandler.shadowUpdate(shadowMessage,on_message, 5)
+print ("Shadow Guncelleme Gonderildi")
+shadowClient.disconnect()
+print ("Baglandi Kesildi")
+time.sleep(2)
---
+++ Scripts/lab3/lab3-2.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python
+
+# Lab 3-2
+# AWS Region Ireland olduguna emin olun.
+
+import sys
+import ssl
+from AWSIoTPythonSDK.MQTTLib import AWSIoTMQTTShadowClient
+import json
+import time
+import os 
+import random
+
+#MQTT Shadow Istemci olusturma ve guvenlik sertifikalarini tanimlama
+#Sertifa isimleri ile AWS IoT Thing indirdiginiz sertifika isimlerinin ayni olmasina dikkat edin.
+
+currentPath=os.getcwd()
+roboName=os.path.basename(currentPath)
+print("RoboName--> "+roboName)
+
+shadowClient=AWSIoTMQTTShadowClient(roboName)
+
+#Ayarlar sayfasindaki IoTEndpoint buraya ekleyin
+shadowClient.configureEndpoint("a100y3wur4j1gq-ats.iot.eu-west-1.amazonaws.com",8883)
+shadowClient.configureCredentials("../root-CA.crt","PrivateKey.pem","certificate.pem.crt")
+
+shadowClientHandler=shadowClient.createShadowHandlerWithName("Robo1",True) # Robo1 Thing adi olarak statik tanimli, parametre de olabilir.
+#JSON formatina encode eden fonksiyon
+def toJSON(string):
+        return json.dumps(string)
+
+shadowClient.toJSON=toJSON
+
+#Function to encode a payload into JSON
+def json_encode(string):
+        return json.dumps(string)
+
+shadowClient.json_encode=json_encode
+shadowClientHandler.json_encode=json_encode
+
+def on_message(message, response, token):
+    print (message)
+
+shadowClient.on_message = on_message
+shadowClientHandler.on_message= on_message
+
+shadowClient.connect()
+print ("Baglandi")
+shadowClientHandler.shadowRegisterDeltaCallback(on_message) #Delta degerini okuyan method.
+print ("Delta Mesajlarini Dinliyorum")
+
+# Sonsuz Dongu
+while True:
+        pass
